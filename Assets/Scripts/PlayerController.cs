c3ff1214f2eedbd29eb127dcf8756457c779080d
Gabe Gomez <gomezagabriel2@gmail.com>
2022-09-25 22:27:42 -0500

    Got some good movement going so far

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class PlayerController : MonoBehaviour
     Quaternion rot;
     bool isRun;
 
+    Vector3 startingMousePos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,39 +27,76 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Rotate();
+        Move();
         transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        if (!isJump) {
+            Jump();
+        }
     }
 
     void Rotate()
     {
-        if (Input.GetKey(KeyCode.D))
-        {            
-            Move();            
-            rot = Quaternion.LookRotation(Vector3.right);
+        if (Input.GetMouseButtonDown(1)) {
+            startingMousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         }
+
+        if (Input.GetMouseButton(1)) {
+            Vector3 currentMousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            Vector3 targetDir = currentMousePos - startingMousePos;
+
+            Vector3 currentEuler = transform.rotation.eulerAngles;
+
+            currentEuler.y = currentEuler.y - ( targetDir.x * speed_rot );
+
+            rot = Quaternion.Euler(currentEuler);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            Vector3 currentEuler = transform.rotation.eulerAngles;
+
+            currentEuler.y = currentEuler.y + ( speed_rot / 2 );
+
+            rot = Quaternion.Euler(currentEuler);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            Vector3 currentEuler = transform.rotation.eulerAngles;
+
+            currentEuler.y = currentEuler.y - ( speed_rot / 2);
+
+            rot = Quaternion.Euler(currentEuler);
+        }
+        
+        // if (Input.GetKey(KeyCode.D))
+        // {            
+        //     Move();            
+        //     rot = Quaternion.LookRotation(Vector3.right);
+        // }
 
         
-        else if (Input.GetKey(KeyCode.A))
-        {            
-            Move();
-            rot = Quaternion.LookRotation(Vector3.left);
-        }
+        // else if (Input.GetKey(KeyCode.A))
+        // {            
+        //     Move();
+        //     rot = Quaternion.LookRotation(Vector3.left);
+        // }
 
-        else if (Input.GetKey(KeyCode.W)) {
-            Move();
-            rot = Quaternion.LookRotation(Vector3.forward);
-        }
+        // else if (Input.GetKey(KeyCode.W)) {
+        //     Move();
+        //     rot = Quaternion.LookRotation(Vector3.forward);
+        // }
 
-        else if (Input.GetKey(KeyCode.S)) {
-            Move();
-            rot = Quaternion.LookRotation(Vector3.back);
-        }
+        // else if (Input.GetKey(KeyCode.S)) {
+        //     Move();
+        //     rot = Quaternion.LookRotation(Vector3.back);
+        // }
 
-        else
-        {            
-            anim.SetBool("Run", false);
-                anim.SetBool("Walk", false);
-        }
+        // else
+        // {            
+        //     anim.SetBool("Run", false);
+        //         anim.SetBool("Walk", false);
+        // }
 
         transform.rotation = Quaternion.Slerp(transform.rotation, rot, speed_rot * Time.deltaTime);
 
@@ -64,17 +104,57 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        if (isJump)
-        {            
-            transform.position += transform.forward * speed_move * Time.deltaTime;            
-            anim.SetBool("Run", false);
+        if (Input.GetKey(KeyCode.W)) {
+            if (isJump)
+            {            
+                transform.position += transform.forward * speed_move * Time.deltaTime;            
+                anim.SetBool("Run", false);
                 anim.SetBool("Walk", false);
 
-        }
-        else
-        {            
-            anim.SetBool("Run", true);
+            }
+            else
+            {            
+                anim.SetBool("Run", true);
                 anim.SetBool("Walk", Input.GetKey(KeyCode.LeftShift));
+            }
         }
+
+        else if (Input.GetKey(KeyCode.S)) {
+            if (isJump)
+            {            
+                transform.position -= transform.forward * ( speed_move / 2 ) * Time.deltaTime;            
+                anim.SetBool("WalkBack", false);
+                anim.SetBool("Walk", false);
+
+            }
+            else
+            {            
+                anim.SetBool("WalkBack", true);
+            }
+        }
+
+        else {
+            anim.SetBool("Run", false);
+            anim.SetBool("Walk", false);
+            anim.SetBool("WalkBack", false);
+        }
+    }
+
+    void Jump()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {            
+            anim.SetBool("Block", false);
+            anim.SetBool("Crouch", false);
+            anim.SetTrigger("Jump");
+
+            isJump = true;
+        }
+    }
+
+    void JumpEnd()
+    {
+        isJump = false;
     }
 }
